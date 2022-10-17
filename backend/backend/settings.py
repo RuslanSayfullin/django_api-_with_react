b267cf858ef45a45e2a95152cd6afebaa12deb20
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-10-17 09:51:32 +0500

    Authentication – Sign Up.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'appmain.apps.AppmainConfig',
     'api.apps.ApiConfig',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -107,3 +108,9 @@ FILE_UPLOAD_MAX_MEMORY_SIZE = 157286400    # Макс. размер (в байт
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
