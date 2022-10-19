61a3a6eb64e7ed35828bf26aeea30ee42860b0af
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-10-19 09:58:57 +0500

    Defining Our Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -51,6 +51,48 @@ function App() {
                     </Nav>
                 </div>
             </Navbar>
+            <div className="container mt-4">
+                <Switch>
+                    <Route exact path={["/", "/todos"]} render={(props) =>
+                        <TodosList {...props} token={token} />
+                    }>
+                    </Route>
+                        <Route path="/todos/create" render={(props)=>
+                        <AddTodo {...props} token={token} />
+                    }>
+                    </Route>
+                        <Route path="/todos/:id/" render={(props)=>
+                        <AddTodo {...props} token={token} />
+                    }>
+                    </Route>
+                        <Route path="/login" render={(props)=>
+                        <Login {...props} login={login} />
+                    }>
+                    </Route>
+                        <Route path="/signup" render={(props)=>
+                        <Signup {...props} signup={signup} />
+                    }>
+                    </Route>
+                </Switch>
+            </div>
+            <footer className="text-center text-lg-start
+                bg-light text-muted mt-4">
+                <div className="text-center p-4">
+                    © Copyright - <a
+                    target="_blank"
+                    className="text-reset fw-bold text-decoration-none"
+                    href="https://twitter.com/greglim81"
+                >
+                    Greg Lim
+                    </a> - <a
+                        target="_blank"
+                        className="text-reset fw-bold text-decoration-none"
+                        href="https://twitter.com/danielgarax"
+                    >
+                        Daniel Correa
+                    </a>
+                </div>
+            </footer>
         </div>
     );
 }
