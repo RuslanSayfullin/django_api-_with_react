6947bc12b01bf09fe4c27a2f2d5b7885746d1d47
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-10-21 09:51:45 +0500

    TodosList Component

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = secret_key
 
-ALLOWED_HOSTS = ['127.0.0.1', 'cryptolis.ru', 'cryptolis.online', '80.78.244.196']
+ALLOWED_HOSTS = ['localhost:8000', '127.0.0.1', 'cryptolis.ru', 'cryptolis.online', '80.78.244.196']
 INTERNAL_IPS = ('127.0.0.1',)   # кортеж с перечнем IP-адресов, с которых может вестись разработка.
 
 
